cc015a29a43b5eae287bd27e453d1c0fea2cebbb
prindarh <prindar@163.com>
2018-02-28 16:27:38 +0800

    sc

---
+++ static/config.js
@@ -0,0 +1,8 @@
+// export const BASEURL='http://192.168.21.31:8080/aobo'
+export const BASEURL='http://result.eolinker.com/H3zvIFmfb84d1f49ce545abab63ab3fdd24e337cbe7eacc?uri='
+// export const BASEURL='http://192.168.20.145:8080/aobo'
+// export const BASEURL='http://192.168.20.153:8080/aobo'
+//    export const BASEURL='/aobo'
+export const isStatic=true
+export const BASEURLMOCK=''
+export const TIMEOUT=30000
